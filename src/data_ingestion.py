8f747a1fcceb9facffcecfa06de69ba4703f4e2b
kira2406 <kush.p030.24@gmail.com>
2025-03-20 00:47:42 -0400

    Added training pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from google.cloud import storage
 from src.logger import get_logger
-from custom_exception import CustomException
+from src.custom_exception import CustomException
 from config.paths_config import *
 from utils.common_functions import read_yaml
 
